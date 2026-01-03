78626319bac1ff511cde9f2454d058745bd365b1
Karim Mahfouz <karimmahfouznd@gmail.com>
2026-01-03 05:37:27 -0500

    clean up formatting and remove unused files

--- src/App.jsx
+++ src/App.jsx
@@ -6,43 +6,43 @@ import './App.css'
 function App() {
 
   return (
-    
-      <div className='page-container'>
 
-        <div className='logo-container'>
-          <img src={"/mothersLogo.jpeg"} className="logo react" alt="React logo" />
+    <div className='page-container'>
 
-        </div>
-        <h1>Dolab Bakiza</h1>
-        <h2>Coming soon!</h2>
-        <div className='card-container'>
-          <a href="https://www.instagram.com/dolab.bakiza/" target='_blank' rel="noopener noreferrer">
-            <div className="card" style={{
-              backgroundImage: `url(/instagramLogo.png)`,
-            }}></div></a>
+      <div className='logo-container'>
+        <img src={"/mothersLogo.jpeg"} className="logo react" alt="React logo" />
 
-          <a href="https://www.facebook.com/dolab.bakiza.2025" target='_blank' rel="noopener noreferrer"><div className="card" style={{
-            backgroundImage: `url(/facebookLogo.png)`,
+      </div>
+      <h1>Dolab Bakiza</h1>
+      <h2>Coming soon!</h2>
+      <div className='card-container'>
+        <a href="https://www.instagram.com/dolab.bakiza/" target='_blank' rel="noopener noreferrer">
+          <div className="card" style={{
+            backgroundImage: `url(/instagramLogo.png)`,
           }}></div></a>
 
-          <a href="https://api.whatsapp.com/send?phone=00201205253142" target='_blank' rel="noopener noreferrer">
+        <a href="https://www.facebook.com/dolab.bakiza.2025" target='_blank' rel="noopener noreferrer"><div className="card" style={{
+          backgroundImage: `url(/facebookLogo.png)`,
+        }}></div></a>
 
-            <div className="card" style={{
-              backgroundImage: `url("/whatsappLogo.png")`,
-            }}></div>
-          </a>
+        <a href="https://api.whatsapp.com/send?phone=00201205253142" target='_blank' rel="noopener noreferrer">
 
-          <a href="https://www.tiktok.com/@dolab.bakiza" target='_blank' rel="noopener noreferrer">
-            <div className="card" style={{
-              backgroundImage: `url(/tiktokLogo.png)`,
-            }}></div>
-          </a>
+          <div className="card" style={{
+            backgroundImage: `url("/whatsappLogo.png")`,
+          }}></div>
+        </a>
+
+        <a href="https://www.tiktok.com/@dolab.bakiza" target='_blank' rel="noopener noreferrer">
+          <div className="card" style={{
+            backgroundImage: `url(/tiktokLogo.png)`,
+          }}></div>
+        </a>
 
 
-        </div>
       </div>
+    </div>
 
-   
+
   )
 }
 
